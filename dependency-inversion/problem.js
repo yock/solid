052d7d7cc9b883bfbbd41f4e0cb161e81c4452d4
Michael Yockey <mike@heysparkbox.com>
2019-08-14 17:12:06 -0400

    Simpler DI example

--- dependency-inversion/problem.js
+++ dependency-inversion/problem.js
@@ -1,44 +1,5 @@
-const ENV = {
-  baseURL: 'http://example.com'
-}
-
-class WebServiceClient {
-  get(request) {
-    const response = fetch(request.getWebAPIRequest());
-    return request.extractResponse(request);
-  }
-}
-
-class WebServiceRequest {
-  buildInstance(obj) {}
-
-  extractResponse(response) {
-    this.logResponse(response);
-    const obj = JSON.parse(response.body); 
-    return buildInstance(obj);
-  }
-
-  logResponse(response) {
-    // logging code...
-  }
-}
-
-class ProfileRequest extends WebServiceRequest {
-  constructor(authToken) {
-    this.authToken = authToken;
-  }
-
-  getWebAPIRequest() {
-    const headers = new Headers();
-    headers.append('Authorization', `Bearer ${this.authToken}`);
-    return new Request(`${ENV['baseURL']}/profile`, { headers });
-  }
-
-  extractResponse(response) {
-    this.logResponse(response);
-    const obj = JSON.parse(response.body); 
-    const profileVersion = response.headers['X-Profile-Version'];
-    const profile = obj.profile;
-    return new Profile(profile.name, profile.birthdate, profile.accessLevel, profileVersion);
-  }
+const getRemoteProfile(authToken) {
+  const client = new WebServiceClient();
+  const profileRequest = new ProfileRequest(ENV['baseURL'], authToken);
+  return client.get(profileRequest);
 }
--- dependency-inversion/solution.js
+++ dependency-inversion/solution.js
@@ -1,40 +1,4 @@
-const localDevInstances = {
-  client: new MockWebServiceClient(),
-  profileRequestFactory: new ProfileRequestFactory('http://example.com')
-};
-
-const productionInstances = {
-  client: new WebServiceClient(),
-  profileRequestFactory: new ProfileRequestFactory('http://api.seesparkbox.com')
-};
-
-class Container {
-  constructor(instances) {
-    this.instances = instances;
-  }
-
-  getInstanceOf(name) {
-    if (this.instances[name]) {
-      return this.instances[name];
-    }
-    throw 'No instance by that name';
-  }
+const getRemoteProfile(authToken, client = new WebServiceClient()) {
+  const profileRequest = new ProfileRequest(ENV['baseURL'], authToken);
+  return client.get(profileRequest);
 }
-
-class ProfileRequestFactory {
-  constructor(baseURL) {
-    this.baseURL = baseURL;
-  }
-
-  getInstance(authToken) {
-    return new ProfileRequest(this.baseURL, authToken);
-  }
-}
-
-const container = new Container(productionInstances);
-
-const client = container.getInstanceOf('client');
-const authToken = 'token'; // From a request we haven't implemented
-const profileRequest = container.getInstanceof('profileRequestFactory').getInstance(authToken);
-const profile = client.get(profileRequest);
-
