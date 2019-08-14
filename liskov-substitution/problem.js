c4bf9fac42b3b84fcdcdab2c235561943b369397
Michael Yockey <mike@heysparkbox.com>
2019-08-14 17:55:52 -0400

    Simplified Liskov example

--- liskov-substitution/problem.js
+++ liskov-substitution/problem.js
@@ -1,27 +1,11 @@
 class Request {
-  path() {}
-  buildInstance(obj) {}
-
   extractResponse(response) {
     this.logResponse(response);
     const obj = JSON.parse(response.body); 
     return buildInstance(obj);
   }
 
-  logResponse(response) {
-    // elided
-  }
-}
-
-class SomethingElseRequest extends Request {
-  path() {
-    return '/something-else';
-  }
-
-  buildInstance(obj) {
-    const somethingElse = obj.somethingElse;
-    return new SomethingElse(somethingElse.isAmazing, somethingElse.score);
-  }
+  logResponse(response) {/*...*/}
 }
 
 class ProfileRequest extends Request {
